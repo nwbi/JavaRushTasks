da8a55a143554d17a34199ee6678e35b21c1270c
Iurii <yuril@list.ru>
2017-12-27 16:22:06 +0300

    JavaRush tasks level 5 + 0519

--- 1.JavaSyntax/src/com/javarush/task/task05/task0519/Circle.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0519/Circle.java
@@ -8,14 +8,27 @@ package com.javarush.task.task05.task0519;
 public class Circle {
     int centerX, centerY, radius, width, color;
 
-    public Circle(int centerX,  int centerY){
-        this.centerX
+    public Circle(int centerX,  int centerY, int radius){
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
     }
-    /*
-    2. У класса должен быть конструктор, принимающий в качестве параметров centerX, centerY, radius и инициализирующий соответствующие переменные класса.
-3. У класса должен быть конструктор, принимающий в качестве параметров centerX, centerY, radius, width и инициализирующий соответствующие переменные класса.
-4. У класса должен быть конструктор, принимающий в качестве параметров centerX, centerY, radius, width, color и инициализирующий соответствующие переменные класса.
-     */
+
+    public Circle(int centerX,  int centerY, int radius, int width){
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+        this.width = width;
+    }
+
+    public Circle(int centerX,  int centerY, int radius, int width, int color){
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+        this.width = width;
+        this.color = color;
+    }
+        
     //напишите тут ваш код
 
     public static void main(String[] args) {
